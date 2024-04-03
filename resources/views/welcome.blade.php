082bd2a4844bacf96693ade74d0713ad7256767e
Jeb <hermosillajebzenneth@gmail.com>
2024-04-03 13:09:10 +0800

    creating the website

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+@stack('css')
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UV-MTC</title>
+    <link  rel="stylesheet" type="text/css"  href="{{URL::asset('cssfile/main.css')}}">
+</head>
+<body>
+    <header>
+    <h1>PortFolio</h1>
+    </header>
+    <nav>
+        <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#news">News</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li style="float:right"><a class="active" href="#about">About</a></li>
+        </ul>
+    </nav>
+    <banner>
+
+    </banner>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+
+</body>
+</html>
